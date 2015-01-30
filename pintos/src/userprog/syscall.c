3bb9fb4b7d6b3a2ce23266250d17936c12364ef6
Elizabeth Davis <elizabethdavis@stanford.edu>
2015-01-30 02:13:41 -0800

    first update

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,6 +12,139 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+/* Terminates Pintos by calling power_off() (declared in "threads/init.h").
+   This should be seldom used, because you lose some information about 
+    possible deadlock situations, etc. */
+void 
+syscall_halt (void) 
+{
+    /* NOT YET IMPLEMENTED */
+}
+
+/* Terminates the current user program, returning status to the kernel. If the
+   process's parent waits for it (see below), this is the status that will be
+   returned. Conventionally, a status of 0 indicates success and nonzero values
+   indicate errors. */
+void
+syscall_exit (int status UNUSED)
+{
+  /* NOT YET IMPLEMENTED */
+}
+
+/* Runs the executable whose name is given in cmd_line, passing any given
+   arguments, and returns the new process's program id (pid). Must return pid
+   -1, which otherwise should not be a valid pid, if the program cannot load or
+   run for any reason. Thus, the parent process cannot return from the exec
+   until it knows whether the child process successfully loaded its executable.
+   You must use appropriate synchronization to ensure this. */
+pid_t
+syscall_exec (const char *cmd_line UNUSED)
+{
+  /* NOT YET IMPLEMENTED */
+  return -1;
+}
+
+/* Waits for a child process pid and retrieves the child's exit status.
+
+   If pid is still alive, waits until it terminates. Then, returns the status
+   that pid passed to exit. If pid did not call exit(), but was terminated by
+   the kernel (e.g. killed due to an exception), wait(pid) must return -1. It
+   is perfectly legal for a parent process to wait for child processes that
+   have already terminated by the time the parent calls wait, but the kernel
+   must still allow the parent to retrieve its child's exit status, or learn
+   that the child was terminated by the kernel. */
+int
+syscall_wait (pid_t pid UNUSED)
+{
+  /* NOT YET IMPLEMENTED */
+  return -1;
+}
+
+/* Creates a new file called file initially initial_size bytes in size. Returns
+   true if successful, false otherwise. Creating a new file does not open it:
+   opening the new file is a separate operation which would require a open
+   system call. */
+bool
+syscall_create (const char *file UNUSED, uint32_t initial_size UNUSED)
+{
+  /* NOT YET IMPLEMENTED */
+  return false;
+}
+
+/* Deletes the file called file. Returns true if successful, false otherwise. A
+   file may be removed regardless of whether it is open or closed, and removing
+   an open file does not close it. See Removing an Open File, for details. */
+bool
+syscall_remove (const char* file UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+  return false;
+}
+
+/* Opens the file called file. Returns a nonnegative integer handle called a
+   "file descriptor" (fd), or -1 if the file could not be opened. */
+int
+syscall_open (const char* file UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+  return -1;
+}
+
+/* Returns the size, in bytes, of the file open as fd. */
+int
+syscall_filesize (int fd UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+  return 0;
+}
+
+/* Reads size bytes from the file open as fd into buffer. Returns the number of
+   bytes actually read (0 at end of file), or -1 if the file could not be read
+   (due to a condition other than end of file). Fd 0 reads from the keyboard
+   using input_getc(). */
+int
+syscall_read (int fd UNUSED, void *buf UNUSED, uint32_t size UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+  return -1;
+}
+
+/* Writes size bytes from buffer to the open file fd. Returns the number of
+   bytes actually written, which may be less than size if some bytes could not
+   be written. */
+int
+syscall_write (int fd UNUSED, const void* buf UNUSED, uint32_t size UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+  return 0;
+}
+
+/* Changes the next byte to be read or written in open file fd to position,
+   expressed in bytes from the beginning of the file. */
+void
+syscall_seek (int fd UNUSED, uint32_t position UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+}
+
+/* Returns the position of the next byte to be read or written in open file fd,
+   expressed in bytes from the beginning of the file. */
+uint32_t
+syscall_tell (int fd UNUSED) 
+{
+   /* NOT YET IMPLEMENTED */
+  return 0;
+}
+
+/* Closes file descriptor fd. Exiting or terminating a process implicitly
+   closes all its open file descriptors, as if by calling this function for
+   each one. */
+void
+syscall_close (int fd UNUSED)
+{
+   /* NOT YET IMPLEMENTED */
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
