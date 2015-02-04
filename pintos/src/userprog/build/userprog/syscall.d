4cbe278edbd853fb333aef31b0dd2787e79975e1
J Evans <jevans2@corn26.stanford.edu>
2015-02-04 00:28:14 -0800

    Committed

---
+++ pintos/src/userprog/build/userprog/syscall.d
@@ -0,0 +1,9 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../lib/stdint.h ../../userprog/process.h ../../filesys/file.h \
+ ../../filesys/off_t.h ../../threads/thread.h ../../lib/debug.h \
+ ../../lib/kernel/list.h ../../lib/stdbool.h ../../lib/stddef.h \
+ ../../threads/fixed-point.h ../../lib/kernel/hash.h \
+ ../../lib/kernel/list.h ../../lib/stdio.h ../../lib/stdarg.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
+ ../../threads/interrupt.h ../../filesys/filesys.h \
+ ../../devices/shutdown.h ../../lib/kernel/hash.h
