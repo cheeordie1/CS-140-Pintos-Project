bd1d0bd3056e194dbda893fed3c7584d728f2025
cheeordie1 <cheeawai@stanford.edu>
2015-03-10 00:40:54 -0700

    added more cache stuff hoorah

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -1,9 +1,12 @@
 #include "filesys/cache.h"
 #include <bitmap.h>
 #include <hash.h>
+#include <string.h>
 
 #define CACHE_MAX 64
 #define CACHE_BLOCK_SZ 512
+
+struct inode_disk;
 
 struct buffer_cache
   {
@@ -14,11 +17,15 @@ struct buffer_cache
 struct cache_block
   {
     struct hash_elem elem;
-    size_t inumber;
-    char inode_chunk[CACHE_BLOCK_SZ];
+    block_sector_t inode_sector;
+    char dir_name[NAME_MAX + 1];
+    struct inode_disk data;
   };
 
 static unsigned cache_hash (const struct hash_elem *e, void *aux);
+static bool cache_cmp (const struct hash_elem *a,
+                       const struct hash_elem *b,
+                       void *aux);
 
 static struct buffer_cache cache;
 
@@ -27,7 +34,8 @@ cache_init ()
 {
   lock_init (&cache_lock);
   cache.used_blocks = bitmap_create (CACHE_BLOCKS);
-  cache.cache_segment = hash_init (&cache_segment);
+  cache.cache_segment = hash_init (&cache_segment, cache_hash, 
+                                   cache_cmp, NULL);
 }
 
 /* Hash the cache block by inumber. */
@@ -38,19 +46,40 @@ cache_hash (const struct hash_elem *e, void *aux)
 }
 
 /* Compare cache blocks by inumber. */
-bool
+static bool
 cache_cmp (const struct hash_elem *a,
            const struct hash_elem *b,
            void *aux)
 {
   struct cache_block *block_a = hash_entry (a, struct cache_block, elem);
   struct cache_block *block_b = hash_entry (b, struct cache_block, elem);
-  return block_a->inumber < block_b->inumber;
+  return (block_a->inumber < block_b->inumber) || 
+         (strcmp (block_a->name, block_b->name) < 0);
+}
+
+struct inode *
+cache_find_inode (block_sector_t inode_sector, char *name)
+{
+  struct cache_block singleton;
+  singleton.inode_sector = inode_sector;
+  if (name != NULL)
+    strlcpy (singleton.dir_name, name, NAME_MAX);
+  else
+    singleton.dir_name[0] = '\0';
+  struct hash_elem *cached_inode = hash_find (&cache.cache_segment, 
+                                              &singleton.elem);
+  if (hash_elem == NULL)
+    return NULL;
+  else
+    return &hash_entry (cached_inode, struct cache_block, elem)->data;
 }
 
 /* Insert an inode into the cache based on inumber. 
    If out of room, evict. */
-void cache_insert ()
+void 
+cache_insert (struct cache_block *cache_block)
 {
   
 }
+
+
--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -2,9 +2,12 @@
 #define CACHE_H
 
 #include "threads/synch.h"
+#include "devices/block.h"
 
 struct lock *cache_lock;
 
 void cache_init (void);
+struct inode *cache_find_inode (block_sector_t inode_sector, char *name);
+void cache_insert (struct cache_block *cache_block);
 
 #endif /* filesys/cache.h */
